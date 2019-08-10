098c2110f211afa933c71500263e75eb0f398f42
Litavets <dm.stepanets@gmail,com>
2019-08-10 03:16:31 +0300

    "first steps"

---
+++ src/avaj/Simulator.java
@@ -0,0 +1,30 @@
+package avaj;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+
+public class Simulator {
+
+	public Simulator() {
+
+	}
+
+
+	public static void main(String[] args) throws IOException {
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+			String ln = reader.readLine();
+
+			System.out.println("Line: " + ln);
+
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR: I can't find such file, darling :(");
+		}
+
+	}
+
+}
