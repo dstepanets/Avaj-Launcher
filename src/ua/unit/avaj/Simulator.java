599d8a251d5a4b3814e12da243c1c7933180ef2a
Litavets <litavets@gmail.com>
2019-09-10 16:57:50 +0300

    config

--- src/ua/unit/avaj/Simulator.java
+++ src/ua/unit/avaj/Simulator.java
@@ -1,1 +1,52 @@
-package ua.unit.avaj;import ua.unit.avaj.aircrafts.Flyable;import java.io.BufferedReader;import java.io.FileReader;import java.io.IOException;import java.io.FileNotFoundException;import java.util.ArrayList;public class Simulator {	private static WeatherTower tower;	private static ArrayList<Flyable> aircrafts = new ArrayList<>();	public Simulator() {}	public static void main(String[] args) {		if (args.length != 1) {			System.out.println("ERROR: Provide a file path as a single argument");			System.exit(1);		}		try {			BufferedReader reader = new BufferedReader(new FileReader(args[0]));			String ln = reader.readLine();				System.out.println("Line: " + ln);	//			if (ln != null) {				try {					tower = new WeatherTower(Integer.parseInt(ln));				} catch (NumberFormatException e) {					System.out.println("ERROR. Number of simulations (first line of the scenario) must be a positive integer");				}			}		} catch (FileNotFoundException e) {			System.out.println("ERROR: I can't find such file, darling :(");		} catch (IOException e) {			System.out.println("ERROR: File reading failed");		} finally {			System.out.println("<placeholder for finally in main. Close files!!!!>");		}	}}
+package ua.unit.avaj;
+
+import ua.unit.avaj.aircrafts.Flyable;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Simulator {
+
+	private static WeatherTower tower;
+	private static ArrayList<Flyable> aircrafts = new ArrayList<>();
+
+	public Simulator() {}
+
+	public static void main(String[] args) {
+
+		if (args.length != 1) {
+			System.out.println("ERROR: Provide a file path as a single argument");
+			System.exit(1);
+		}
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+			String ln = reader.readLine();
+
+
+				System.out.println("Line: " + ln);	//
+
+			if (ln != null) {
+				try {
+					tower = new WeatherTower(Integer.parseInt(ln));
+				} catch (NumberFormatException e) {
+					System.out.println("ERROR. Number of simulations (first line of the scenario) must be a positive integer");
+				}
+
+
+
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR: I can't find such file, darling :(");
+		} catch (IOException e) {
+			System.out.println("ERROR: File reading failed");
+		} finally {
+			System.out.println("[finally in main]: Close files!!!!");
+		}
+	}
+
+}
